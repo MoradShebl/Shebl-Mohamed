ec1bdaaf59b085d44434b8ba7e0c28286172197a
Morad Shebl <114817417+MoradShebl@users.noreply.github.com>
2025-01-17 10:52:57 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,3 @@
-// window.onload = function() {
-//     window.scrollTo(0, 0);
-//     hidul.style.display = 'inline';
-// }
 
 document.addEventListener('DOMContentLoaded', function () {
     const checkbox = document.getElementById('check');
@@ -9,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const header = document.getElementById('header');
     const hidul = document.getElementById('hidul');
     const listItems = header.querySelectorAll('li');
+    const blur = document.getElementById("blur");
 
     checkbox.addEventListener('change', function () {
         if (this.checked) {
@@ -42,5 +39,17 @@ document.addEventListener('DOMContentLoaded', function () {
                 hidul.style.display = 'none';
             }, 500);
         }
+    });
+
+    header.addEventListener("mouseenter", () => {
+        blur.style.display = "block"; // Make the blur div visible
+        blur.style.animation = "blur 0.5s forwards"; // Trigger the blur animation and retain the final state
+    });
+
+    header.addEventListener("mouseleave", () => {
+        blur.style.animation = "stopblur 0.5s forwards"; // Trigger the stopblur animation and retain the final state
+        setTimeout(() => {
+            blur.style.display = "none"; // Hide the blur div after the animation ends
+        }, 500); // Match the animation duration
     });
 });
